a1002d83d316e0640f14cfbbea0580002a0a7f31
Stephen Kershaw <kershawstephen@gmail.com>
2016-08-30 15:29:00 -0400

    Can't delete files after extracting

--- project.clj
+++ project.clj
@@ -5,10 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]
-                 [com.github.junrar/junrar "0.7"]
                  [pacl "0.1.2-SNAPSHOT"]
-                 [com.github.beothorn/junrar "0.6"]
-                 [org.clojars.bonega/java-unrar "0.5.0"]]
+                 [org.clojure/core.async "0.2.385"]
+                 [com.github.junrar/junrar "0.7"]]
+                 ; Can't find proper syntax to use the following two libraries.
+                 ; [com.github.beothorn/junrar "0.6"]
+                 ; [org.clojars.bonega/java-unrar "0.5.0"]]
   :main ^:skip-aot unpack.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/unpack/core.clj
+++ src/unpack/core.clj
@@ -1,10 +1,10 @@
-; skip download step?
-; http://stackoverflow.com/questions/32742744/how-to-download-a-file-and-unzip-it-from-memory-in-clojure
 (ns unpack.core
   (:gen-class)
   (:require [me.raynes.fs.compression :as fsc]
+            [me.raynes.fs :as fs]
             [clojure.java.io :as io]
-            [clojure.string :as s])
+            [clojure.string :as s]
+            [clojure.core.async :as async])
   (:import [com.github.junrar.testutil ExtractArchive]))
 
 (def dl-directory "zipped-files/")
@@ -23,108 +23,104 @@
    :post [(string? %)]}
   (s/replace file #"-?[0-9]*\..*" ""))
 
-; (defn files-set
-;   "gets a set of unique base file names"
-;   [file-list]
-;   (set (map file-name-base file-list)))
-
 (defn get-file-versions
   "returns a lazy seq of files in folder whose names start with input string"
-  [file-str folder]
-  ; (do (println (str "get-file-versions " file-str " " folder))
+  [folder file-str]
   (filter #(s/starts-with? % file-str) (archives-in-dir folder)))
-
-; (defn better-compare
-;   "compare str lengths, then values, so that abc-12 > abc-2. returns the greater str"
-;   ; consider stripping out "-" if naming system is not consistent
-;   [a b]
-;   (if (= (count a) (count b) )
-;     (if (> (compare a b) 0)
-;       a
-;       b)
-;     (if (> (count a) (count b))
-;       a
-;       b)))
-
-; (defn get-latest-version
-;   "get the latest version of file in folder. Not currently used"
-;   [filestr folder]
-;   (reduce better-compare (get-file-versions filestr folder)))
 
 (defn glv-from-coll
   [coll from]
-  {:pre  [(coll? coll)]
-   :post [(string? %)]}
-  (first (filter #(.exists (io/file (str from %1))) (reverse coll))))
+  ; {:pre  [(coll? coll)]
+   ; :post [(string? %)]}
+(try (loop [c (reverse coll)]
+     (let [f (str from (first c))]
+       (if (and (.exists (io/file f)) (not (.isDirectory (io/file f))))
+           f
+           (do (println "Warning: missing the latest data file:" f)
+               (if-not (nil? (next c))
+                       (recur (next c))
+                       "Warning: no files found.")))))
+(catch Exception e "Warning: no files found.")))
 
 (defn make-dir
   "make directory if it does not exist yet"
-  [dir-name]
-  {:pre  [(string? dir-name)]}
-   ; :post [(.exists (io/file dir-name))]}
-  (#(if-not (and (.exists (io/file %1)) (.isDirectory (io/file %1)))
-    (.mkdir (io/file %1))) dir-name))
+  [dir]
+  {:pre  [(string? dir)]}
+   ; :post [(.exists (io/file dir))]}
+  (if-not (and (.exists (io/file dir)) (.isDirectory (io/file dir)))
+          (.mkdir (io/file dir))))
 
 (defn extract
   "supports .tar, .tar.gz, .gz, .zip; returns target dir, which is used in recursion."
-  [archive from to]
-  {:pre [(.exists (io/file (str from archive)))]}
+  [archive to del]
   (make-dir to)
   (try
     (cond
       (s/ends-with? archive ".zip")
-        (fsc/unzip (str from archive) (str to))
+        (do (fsc/unzip archive to)
+            (println "Extracted" archive)
+            (if (true? del) (fs/delete archive)))
+      ; can't extract rar5. And I cannot suppress the output
       (s/ends-with? archive ".rar")
-        (ExtractArchive/extractArchive (io/file (str from archive)) (io/file (str to)))
+        (do (ExtractArchive/extractArchive (io/file archive) (io/file (str to)))
+            (println "Extracted" archive))
       (s/ends-with? archive ".tar")
-        (do (fsc/untar (str from archive) (str to))
-          (str to (file-name-base archive)))
+        (do (fsc/untar archive (str to))
+            (println "Extracted" archive))
       (s/ends-with? archive ".tar.gz")
-        (do (fsc/gunzip (str from archive) (str to (file-name-base archive) ".tar"))
-          (fsc/untar (str to (file-name-base archive) ".tar") (str to))
-          (io/delete-file (str to (file-name-base archive) ".tar"))
-          (str to (file-name-base archive)))
+        (do (fsc/gunzip archive (str (file-name-base archive) ".tar"))
+            (fsc/untar (str (file-name-base archive) ".tar") to)
+            (io/delete-file (str (file-name-base archive) ".tar"))
+            (println "Extracted" archive)
+            )
       (s/ends-with? archive ".gz")
-        (do (fsc/gunzip (str from archive) (str to))
-          (str to))
+        (do (fsc/gunzip archive (str to))
+            (println "Extracted" archive))
       :else (println "nothing to extract"))
-    (catch Exception e (str "caught exception when trying to extract archive: " (.getMessage e)))))
+    (catch Exception e (str "Warning: unable to extract " archive " due to " e)))
+  ; (if (true? del) (println (fs/delete archive)) (println "false")))
+archive)
 
-; (defn extract-recursive
-;   ([archive from to]
-;   (extract-recursive (extract archive from to)))
-;   ([dir]
-;   (if (archives-in-dir dir)
-;       (map #(extract-recursive % dir dir) (archives-in-dir dir)))))
+(defn dele
+  [coll]
+   (map #(if (true? (:del %)) (fs/delete (glv-from-coll (:files %) (or (:from %) "")))) coll))
 
 (defn extract-files
-  "assumes input is a list of maps:
-  [{:to 'target-dir/' :from 'greatest-common-source-dir/' :files ['path1/file.ext' 'path2/file2.ext' ...]} {...} {...}]
-  where the end-most file is the latest version. Extracts the latest existing version."
+  "Assumes input is a coll of maps:
+    [{:to 'target-dir/'
+      :from 'greatest-common-source-dir/'
+      :files ['path1/file.ext' 'path2/file2.ext' ...]
+      :del true}
+     {...}]
+  where the last file is the latest version. Extracts the latest existing version."
   [coll]
   {:pre [(and (coll? coll) (map #(map? %) coll))]}
-  (pmap #(extract (glv-from-coll (% :files) (or (% :from) "")) (or (% :from) "") (or (% :to) "")) coll))
+  (pmap #(extract (glv-from-coll (:files %) (or (:from %) "")) (or (:to %) "") (:del %)) coll))
 
 (defn extract-folder
-  [from to]
+  [from to starts-with del]
   {:pre [(and (string? from) (string? to))]}
-  (map #(extract % from to) (archives-in-dir from)))
+  (map #(extract (str from %) to del) (get-file-versions from (or starts-with ""))))
 
 (defn -main
-  "see extract-files"
   ([coll]
    (extract-files coll))
-  ([from to]
-   (extract-folder from to)))
+  ([from to & starts-with]
+   (extract-folder from to starts-with)))
 
-  ; [& {:keys [from to] :or {from dl-directory to target-directory}}]
-  ; ; the keys might have an impact on performance
-  ; ; (println (str "   " from "   " to ))
 
-  ; (if (> (count (archives-in-dir from)) 0)
-  ;   (->> (files-set (archives-in-dir from))
-  ;     (map #(get-latest-version % from))
-  ;     ; (map #(extract % from (str to (file-name-base %))))
-  ;     (map #(extract % from to ))
-  ;     (map #(-main :from (str % "/") :to (str % "/")))
-  ;     (doall ))))
+;; defs for testing
+
+; check each file type
+(def a [{:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["testfiles-2.zip"] :del false} {:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["tarta.tar"] :del false} {:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["rar.rar"] :del false} {:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["targz.tar.gz"] :del false}])
+
+; without keys
+(def b [{:files ["testfiles-2.zip"]}])
+; with keys without values
+(def x [{:from "zipped-files/extracted-files/" :to "zipped-files/extracted-files/second-extraction" :files ["testfiles-1.zip"] :del false}])
+
+(def c [{:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["rar.rar" "rar5.rar" "rar6.rar"] :del false}])
+
+(def d [{:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["blablog.zip" "blah.zip" "testfiles-2.zip"] :del false} {:from "zipped-files/extracted-files/" :to "zipped-files/extracted-files/second-extraction" :files ["testfiles-1.zip"] :del false}])
+
+(def e [{:from "zipped-files/" :to "zipped-files/extracted-files/" :files ["rar.rar" "rar5.rar" "rar6.rar"] :del false}])
